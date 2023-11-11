47cbca11b1402df515c806ebd6c22123b2808ea2
Saul Morales <moralespachecopablo@gmail.com.com>
2023-11-11 17:56:55 -0500

    Fixed port number

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.use("/api/tasks", taskRouters);
 
 const port = process.env.PORT || 8585;
 
-app.listen(8585, () => {
-    console.log("Server run in port 8585");
+app.listen(port, () => {
+    console.log(`Server run in port ${port}`);
 })
